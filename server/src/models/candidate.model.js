32236279de5b3c1fc988cd9da30f0ed4b0f6f0fa
malkiberman <559120b@gmail.com>
2026-04-21 15:51:28 +0300

    add coment

--- server/src/models/candidate.model.js
+++ server/src/models/candidate.model.js
@@ -4,12 +4,6 @@ const CandidateSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   phone: { type: String },
-  status: { 
-    type: String, 
-    enum: ['pending', 'submitted', 'analyzed'], 
-    default: 'pending' 
-  },
-  answers: [String], // מערך של התשובות שהומר מקול לטקסט
   analysis: { 
     type: mongoose.Schema.Types.ObjectId, 
     ref: 'Analysis' // קישור לטבלת הניתוח שרותי תמלא
