d4c7771eaf6d8aa24115844e4799a651d791fc3c
malkiberman <559120b@gmail.com>
2026-04-22 22:16:27 +0300

    branch2

--- server/src/routes/analysis.routes.js
+++ server/src/routes/analysis.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const analysisController = require('../controllers/analysis.controller');
+const candidateController = require('../controllers/candidate.controller');
 
 // שליפת ניתוח לפי מזהה מועמד
-router.get('/:candidateId', analysisController.getCandidateByIdWithAnaysis);
+router.get('/:candidateId', candidateController.getCandidateByIdWithAnaysis);
 
 // יצירת ניתוח חדש - ודא שזה השם המדויק בקונטרולר
 router.post('/', analysisController.createAnalysis);
